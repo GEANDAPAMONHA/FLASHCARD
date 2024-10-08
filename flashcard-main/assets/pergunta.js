7242350cd7da80c11f8127deebf293257f334671
GEANDAPAMONHA <144158712+GEANDAPAMONHA@users.noreply.github.com>
2024-10-08 16:51:55 -0300

    dian:08/10/2024

---
+++ flashcard-main/assets/pergunta.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'programação' ,
+    'O que é Python' ,
+    'O Python é uma linguagem de programação' ,
+
+)
+
+criaCartao(
+    'Geografia' ,
+    'Qual é a acapital da França' ,
+    'A capital da França é Paris' ,
+
+)
+
+criaCartao(
+    'Programaçao' ,
+    'O que é uman função' ,
+    'Uma função e um bloco de código qu executa alguma tarefa '
+
+)
